03f368fc1f806af04cf27d3128fa4b846631e13f
puokl <phuoc@sapo.pt>
2023-08-09 10:45:49 +0200

    res.header

--- backend/src/controller/session.controller.ts
+++ backend/src/controller/session.controller.ts
@@ -61,6 +61,11 @@ export async function createUserSessionHandler(req: Request, res: Response) {
     },
     { expiresIn: process.env.REFRESHTOKENTTL } // 1y
   );
+
+  // Set CORS headers to allow requests from the frontend
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+
   // 5. return access & refresh tokens
   res.cookie("accessToken", accessToken, accessTokenCookieOptions);
 
